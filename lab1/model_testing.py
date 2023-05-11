17c0e7bb050819e211088d5295b849b005ec0624
Ig0r_ML <117780098+Zagalskiy@users.noreply.github.com>
2023-05-11 20:54:24 +0300

    Update model_testing.py

--- lab1/model_testing.py
+++ lab1/model_testing.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import pickle
 
+# Загрузка обработанных данных из файлов
+test_data = pd.read_csv('test_preprocessed/test_data_preprocessed.csv')
+
+# Загрузка модели из файла
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Тестирование модели
+X_test = test_data[['temperature', 'humidity', 'pressure']]
+y_test = test_data['label']
+accuracy = model.score(X_test, y_test)
+
+# Вывод результатов тестирования
+print("Model test accuracy is:", accuracy)
