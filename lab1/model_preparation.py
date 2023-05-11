3ccb4f8029b968c4fa3df0be8a34575fc58021e9
Ig0r_ML <117780098+Zagalskiy@users.noreply.github.com>
2023-05-11 20:52:31 +0300

    Update model_preparation.py

--- lab1/model_preparation.py
+++ lab1/model_preparation.py
@@ -1,1 +1,16 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+import pickle
 
+# Загрузка обработанных данных из файлов
+train_data = pd.read_csv('train_preprocessed/train_data_preprocessed.csv')
+
+# Обучение модели
+X_train = train_data[['temperature', 'humidity', 'pressure']]
+y_train = train_data['label']
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Сохранение модели в файл
+with open('model.pkl', 'wb') as f:
+    pickle.dump(model, f)
