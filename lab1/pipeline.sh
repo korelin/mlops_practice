c8c573442dd878a4d0988ca4bd61765e1a426397
Ig0r_ML <117780098+Zagalskiy@users.noreply.github.com>
2023-05-11 20:55:10 +0300

    Update pipeline.sh

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,5 +1,13 @@
-data_creation.py
-data_preprocessing.py
-model_preparation.py
-model_testing.py
-pickle 
+#!/bin/bash
+
+# Шаг 1: Создание данных
+python lab1/data_creation.py
+
+# Шаг 2: Предобработка данных
+python lab1/data_preprocessing.py
+
+# Шаг 3: Подготовка и обучение модели
+python lab1/model_preparation.py
+
+# Шаг 4: Тестирование модели
+python lab1/model_testing.py
