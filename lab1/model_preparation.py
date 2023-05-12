420e71894fa7cf76685dc8ccb2679f54b60857e9
Ig0r_ML <izagalskiy@mail.ru>
2023-05-12 16:47:41 +0300

    Add comments

--- lab1/model_preparation.py
+++ lab1/model_preparation.py
@@ -1,16 +1,22 @@
-import pandas as pd
-from sklearn.linear_model import LogisticRegression
-import pickle
+import pandas as pd # для работы с табличными данными
+from sklearn.linear_model import LogisticRegression # классификатор лог. регрессии
+import pickle # для (де)сериализации объектов
 
-# Загрузка обработанных данных из файлов
+# Загрузка обработанных тренировочных данных из файла train_data_preprocessed.csv
 train_data = pd.read_csv('train_preprocessed/train_data_preprocessed.csv')
 
-# Обучение модели
+# Сохранение признаков температура, влажность, давление
 X_train = train_data[['temperature', 'humidity', 'pressure']]
+
+# Сохранение целевой метки
 y_train = train_data['label']
+
+# Создание модели логистической регрессии
 model = LogisticRegression()
+
+# Обучение модели на тренировочных данных
 model.fit(X_train, y_train)
 
-# Сохранение модели в файл
+# Сохранение модели в файл model.pkl
 with open('model.pkl', 'wb') as f:
     pickle.dump(model, f)
