e11544a4ccf332185d7df8223b6db7aad0f44769
SavSV <sv.ework@gmail.com>
2023-05-13 23:29:36 +0300

    Step 1. Generate data

--- lab1/Lab 1 Group4/data_creation.py
+++ lab1/Lab 1 Group4/data_creation.py
@@ -1,48 +1,59 @@
-# In[ ]: Импорт
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
-from sklearn.datasets import make_moons, make_circles, make_classification
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import cross_validate
-from sklearn.model_selection import ShuffleSplit
+import argparse
+import os
+# %% Импорт
+import sys
 
-pd.set_option("display.max_rows", 20)
-pd.set_option("display.max_columns", 20)
-pd.set_option("display.precision", 6)
-pd.set_option("plotting.backend", "matplotlib")
+from generate_data import make_data, devide_save
+from plot_data import plot_data
 
-plt.style.use('default')
 
-# In[ ]: Генерация
-def make_binary_clf(N, method='line',
-                    noises=0.15,
-                    random_state=42):
-    if random_state: rng = np.random.RandomState(seed=random_state)
+# %% Задание пути для сохранения файлов
+file_path = os.getcwd()
 
-    if method == 'line' or method is None:
-        X, y = make_classification(n_samples=N, n_features=2,
-                                   n_redundant=0, n_informative=2,
-                                   n_clusters_per_class=1, class_sep=2,
-                                   random_state=random_state)
-        X += np.random.randn(*X.shape) * noises
-        out = (X, y)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dir')
+    namespace = parser.parse_args()
 
-    elif method == 'moons':
-        X, y = make_moons(n_samples=N, noise=noises,
-                          random_state=random_state)
+    if namespace.dir:
+        file_path = namespace.dir
 
-    elif method == 'circles':
-        X, y = make_circles(n_samples=N, noise=noises, factor=0.5,
-                            random_state=random_state)
+# Если пути не существует, скрипт прерывает свою работу
+if (not os.path.isdir(file_path)) or \
+        (not os.path.isdir(file_path + 'train/')) or \
+        (not os.path.isdir(file_path + 'test/')):
+    print("There is no such path")
+    sys.exit(1)
 
-    return X, y
+# %% Генерация данных
 
-# In[ ]: Генерация
-N = 150  # @param {type:"slider", min:50, max:250, step:25}
+# Количество данных
+count = 15000
 
-moons_data = make_binary_clf(N, method='moons', noises=0.2)
+# Зерно генератора случайных чисел
+seed = 38
+
+moons_data = devide_save(*make_data(count, method='moons',
+                                    noises=0.15, random_state=seed), file_path, 'moons')
+if moons_data is None:
+    sys.exit(2)
+
+circles_data = devide_save(*make_data(count, method='circles',
+                                      noises=0.15, random_state=seed), file_path, 'circles')
+if circles_data is None:
+    sys.exit(2)
+
+line_data = devide_save(*make_data(count, method='line',
+                                   noises=0.15, random_state=seed), file_path, 'line')
+if line_data is None:
+    sys.exit(2)
+
+# %% Вывод графиков
+show_plots = True
+if show_plots:
+    plot_data(moons_data, circles_data, line_data, 0)
+    plot_data(moons_data, circles_data, line_data, 1)
