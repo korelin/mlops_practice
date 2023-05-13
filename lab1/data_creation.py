48feae1e3b7e2d55d546480d38fc8b2095f8345e
EkaterinaSeveryanka <varelovna@mail.ru>
2023-05-13 23:12:54 +0500

    Changed the files data_creation.py, data_processing.py

--- lab1/data_creation.py
+++ lab1/data_creation.py
@@ -12,30 +12,26 @@ def data_generator(sample_size: int) -> pd.DataFrame:
     feature_B = 30 * np.random.randn(sample_size) + 200
     feature_C = 5 * np.random.randn(sample_size) + 20
 
-    result = 2 * feature_A\
-             + 8 * feature_B\
-             + feature_C\
+    result = 2 * feature_A \
+             + 8 * feature_B \
+             + feature_C \
              + 25 * np.random.randn(sample_size) + 30
 
-    df = pd.DataFrame(list(zip(feature_A, feature_B, feature_C, result)),
-                      columns=['A', 'B', 'C', 'Y'])
+    df = pd.DataFrame({
+        'A': feature_A,
+        'B': feature_B,
+        'C': feature_C,
+        'Y': result})
 
     # Зашумляем какой-нибудь столбец Nan
     idx = np.random.randint(0, sample_size, round(sample_size * 0.1))
-
-    df['A'].iloc[idx] = np.NaN
-
+    df.loc[idx, 'A'] = np.NaN
     return df
 
-
-if not os.path.exists('./train'):
-    os.makedirs('./train')
-
-if not os.path.exists('./test'):
-    os.makedirs('./test')
-
+os.makedirs('./train', exist_ok = True)
 train_data = data_generator(10000)
-train_data.to_csv('./train/train.csv', sep='\t', encoding='utf-8')
+train_data.to_csv('./train/train.csv', sep='\t', encoding='utf-8', index=False)
 
-test_data = data_generator(1000)
-test_data.to_csv('./test/test.csv', sep='\t', encoding='utf-8')
+os.makedirs('./test', exist_ok = True)
+train_data = data_generator(1000)
+train_data.to_csv('./test/test.csv', sep='\t', encoding='utf-8', index=False)
--- lab1/data_processing.py
+++ lab1/data_processing.py
@@ -1,30 +1,31 @@
 import pandas as pd
-
+from sklearn.preprocessing import StandardScaler
 
 def delete_NaNs(df: pd.DataFrame) -> pd.DataFrame:
     '''Чистим датафрейм от Nanов'''
 
     return df.dropna()
 
-
 train = pd.read_csv('./train/train.csv',
                     sep='\t',
                     encoding='utf-8',
                     usecols=['A', 'B', 'C', 'Y'])
-
 train = delete_NaNs(train)
 
-train.to_csv('./train/train.csv',
-             sep='\t',
-             encoding='utf-8')
+scaler = StandardScaler ()
+train_scaler = scaler.fit_transform(train)
+train_scaler_df = pd.DataFrame(train_scaler, columns=train.columns)
+
+train_scaler_df.to_csv('./train/train.csv', sep='\t', encoding='utf-8', index=False)
 
 test = pd.read_csv('./test/test.csv',
-                   sep='\t',
-                   encoding='utf-8',
-                   usecols=['A', 'B', 'C', 'Y'])
-
+                    sep='\t',
+                    encoding='utf-8',
+                    usecols=['A', 'B', 'C', 'Y'])
 test = delete_NaNs(test)
 
-test.to_csv('./test/test.csv',
-            sep='\t',
-            encoding='utf-8')
+scaler = StandardScaler ()
+test_scaler = scaler.fit_transform(test)
+test_scaler_df = pd.DataFrame(test_scaler, columns=test.columns)
+
+test_scaler_df.to_csv('./test/test.csv', sep='\t', encoding='utf-8', index=False)
