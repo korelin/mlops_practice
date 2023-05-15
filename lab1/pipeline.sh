103fea3ed8d8bb768c7152f386a17576af5fb085
gigabyte727 <gigabyte727@gmail.com>
2023-05-15 16:30:09 +0300

    add new branch testing

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-# Шаг 1: Создание данных
-python lab1/data_creation.py
-
-# Шаг 2: Предобработка данных
-python lab1/data_preprocessing.py
-
-# Шаг 3: Подготовка и обучение модели
-python lab1/model_preparation.py
-
-# Шаг 4: Тестирование модели
-python lab1/model_testing.py
+python3 data_creation.py
+python3 data_preprocessing.py
+python3 model_preparation.py
+python3 model_testing.py
