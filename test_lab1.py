d37d45884e6ea746d14f926b5f07c9305c30b50c
Ig0r_ML <117780098+Zagalskiy@users.noreply.github.com>
2023-05-15 23:57:27 +0300

    Update test_lab1.py

--- test_lab1.py
+++ test_lab1.py
@@ -6,13 +6,13 @@ def test_train_data_file_exists():
     assert os.path.exists('lab1/train/train_data.csv'), \
         "File lab1/train/train_data.csv does not exist."
 
-    
+
 # Функция проверки существования файла lab1/test/test_data.csv
 def test_test_data_file_exists():
     assert os.path.exists('lab1/test/test_data.csv'), \
         "File lab1/test/test_data.csv does not exist."
 
-    
+
 # Запуск функций проверки
 if __name__ == '__main__':
     test_train_data_file_exists()
