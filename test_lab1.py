4db344549e537f607a377f2374603e2f0271a94b
Ig0r_ML <117780098+Zagalskiy@users.noreply.github.com>
2023-05-15 23:44:44 +0300

    Update and rename pytest.py to test_lab1.py

--- test_lab1.py
+++ test_lab1.py
@@ -1,12 +1,15 @@
 import os
 
+
 # Функция проверки существования файла lab1/train/train_data.csv
 def test_train_data_file_exists():
-    assert os.path.exists('lab1/train/train_data.csv'), "File train/train_data.csv does not exist."
+    assert os.path.exists('lab1/train/train_data.csv'),
+    "File train/train_data.csv does not exist."
 
 # Функция проверки существования файла lab1/test/test_data.csv
 def test_test_data_file_exists():
-    assert os.path.exists('lab1/test/test_data.csv'), "File test/test_data.csv does not exist."
+    assert os.path.exists('lab1/test/test_data.csv'),
+    "File test/test_data.csv does not exist."
 
 # Запуск функций проверки
 if __name__ == '__main__':
