66ea7fdacfb578a7d565943d57bb2f14369134b7
Tachat Avanesian <tachat_a@yahoo.com>
2023-05-16 23:08:29 +0300

    Change comments

--- lab1/model_testing.py
+++ lab1/model_testing.py
@@ -2,14 +2,14 @@ import pandas as pd
 import pickle
 
 
-# Загрузка предобработанных тестовых данных из файлов
+# Загрузка предобработанных тестовых данных из файла
 test_data = pd.read_csv('test_preprocessed/test_data_preprocessed.csv')
 
-# Загрузка модели из файла
+# Загрузка модели из файла model.pkl
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-# Тестирование модели
+# Оценка модели на тестовых данных
 X_test = test_data[['temperature', 'humidity', 'pressure']]
 y_test = test_data['label']
 accuracy = model.score(X_test, y_test)
