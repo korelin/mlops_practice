ece79e1ab8550ff325a790e2958204ba0402a3ca
Виктор Ротерман <viktorroterman@MBP-Viktor.lan>
2023-05-29 21:19:58 +0500

    add py file for project in lab2

---
+++ lab2/model_preparation.py
@@ -0,0 +1,113 @@
+import os
+import tensorflow as tf
+
+
+#print("model_preparation.py dummy output")
+
+
+initial_path = os.getcwd()
+working_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(working_path)
+
+TRAIN_FOLDER = "train"
+MODEL_FOLDER = "xception_network_cats_and_dogs"
+CHECKPOINTS_FOLDER = "xception_network_cats_and_dogs_checkpoints"
+
+IMAGE_SIZE = (180, 180)
+BATCH_SIZE = 64
+EPOCHS = 20
+
+# Очищаем workspace от сохранённых обученных моделей от предыдущих запусков.
+os.system("rm -rf {0}".format(CHECKPOINTS_FOLDER))
+os.system("rm -rf {0}".format(MODEL_FOLDER))
+
+
+training_images = tf.keras.utils.image_dataset_from_directory(
+    TRAIN_FOLDER,
+    labels='inferred',
+    image_size = IMAGE_SIZE,
+    batch_size = BATCH_SIZE,
+)
+
+
+# Буферизируем ввод.
+training_images = training_images.prefetch(buffer_size = BATCH_SIZE)
+
+
+# Метод для создания модели Xception network.
+def build_xception_network(input_shape, num_classes):
+    inputs = tf.keras.Input(shape = input_shape)
+
+    # Наращивание можно сделать частью архитектуры нейронной сети.
+    #x = augmentation_network(inputs)
+
+    x = tf.keras.layers.experimental.preprocessing.Rescaling(1.0 / 255)(inputs)
+    x = tf.keras.layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
+
+    x = tf.keras.layers.Conv2D(64, 3, padding="same")(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
+
+    previous_block_activation = x
+
+    for size in [128, 256, 512, 728]:
+        x = tf.keras.layers.Activation("relu")(x)
+        x = tf.keras.layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+
+        x = tf.keras.layers.Activation("relu")(x)
+        x = tf.keras.layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+
+        x = tf.keras.layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        residual = tf.keras.layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = tf.keras.layers.add([x, residual])
+        previous_block_activation = x
+
+    x = tf.keras.layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
+
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+
+    x = tf.keras.layers.Dropout(0.5)(x)
+    outputs = tf.keras.layers.Dense(units, activation=activation)(x)
+    return tf.keras.Model(inputs, outputs)
+
+
+# Создаём модель.
+xception_network_cats_and_dogs = build_xception_network(input_shape = IMAGE_SIZE + (3,), num_classes = 2)
+
+
+# Обучаем нейронную сеть.
+
+# callback для сохранения результатов обучения в формате Keras (h5) после каждой эпохи обучения.
+callbacks = [
+    tf.keras.callbacks.ModelCheckpoint(CHECKPOINTS_FOLDER+"/epoch_{epoch}.h5")
+]
+xception_network_cats_and_dogs.compile(
+    optimizer=tf.keras.optimizers.Adam(1e-3),
+    loss="binary_crossentropy",
+    metrics=["accuracy"],
+)
+model_history = xception_network_cats_and_dogs.fit(
+    training_images, epochs=EPOCHS, callbacks=callbacks
+)
+
+
+# Сохраняем модель целиком в формате TensorFlow (saved_model.pb + variables/).
+xception_network_cats_and_dogs.save(MODEL_FOLDER);
+
+
+os.chdir(initial_path)
