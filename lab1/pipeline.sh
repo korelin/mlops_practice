e718e0ae8bc67e127a62eeea4ddacacb826199ad
kv49 <kv4997@gmail.com>
2023-06-05 02:01:17 +0300

    lab1.4 part2

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -6,4 +6,4 @@ pip install tensorflow Pillow
 python3 "$scriptdir/data_creation.py" > /dev/null 2>&1
 python3 "$scriptdir/data_preprocessing.py" > /dev/null 2>&1
 python3 "$scriptdir/model_preparation.py" > /dev/null 2>&1
-python3 -W ignore "$scriptdir/model_testing.py"
+python3 -W ignore "$scriptdir/model_testing.py" 2>/dev/null
