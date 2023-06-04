0b8a9716947f6edb225262352f5192d30b23d5b1
kv49 <kv4997@gmail.com>
2023-06-05 00:12:07 +0300

    part of lab1.4

--- lab1/model_testing.py
+++ lab1/model_testing.py
@@ -1,1 +1,37 @@
-print("model_testing.py dummy output")
+import os
+import tensorflow as tf
+
+
+#print("model_testing.py dummy output")
+
+
+initial_path = os.getcwd()
+working_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(working_path)
+
+TRAIN_FOLDER = "train"
+MODEL_FOLDER = "xception_network_cats_and_dogs"
+CHECKPOINTS_FOLDER = "xception_network_cats_and_dogs_checkpoints"
+
+IMAGE_SIZE = (180, 180)
+BATCH_SIZE = 64
+
+validation_images = tf.keras.utils.image_dataset_from_directory(
+    TRAIN_FOLDER,
+    labels='inferred',
+    image_size = IMAGE_SIZE,
+    batch_size = BATCH_SIZE,
+)
+
+# Буферизируем ввод.
+validation_images = validation_images.prefetch(buffer_size=BATCH_SIZE)
+
+
+# загружаем заранее обученную модель целиком из формата TensorFlow (saved_model.pb + variables/).
+# в тензорфлоу выгружается и структура нейросети и коэффициенты
+xception_network_cats_and_dogs = tf.keras.models.load_model(MODEL_FOLDER)
+
+xception_network_cats_and_dogs.evaluate(validation_images)
+
+
+os.chdir(initial_path)
