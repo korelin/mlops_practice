cdd8a47c031e5ed38dd9cc30cc998ac79c0950cf
Roman <121679425+RomRomich@users.noreply.github.com>
2023-06-16 09:35:53 +0300

    Update model_testing.py

--- lab2/model_testing.py
+++ lab2/model_testing.py
@@ -1,1 +1,12 @@
+import os
+import pickle
+import pandas as pd
 
+# Загружаем предобработанные данные тестовой выборки.
+test_data = pd.read_csv('test/preprocessed_data.csv')
+
+# Загружаем модель и оцениваем ее результативность на тестовой выборке.
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+metric_value = model.score(test_data[['x']], test_data['y'])
+print(f"Model test accuracy is: {metric_value:.3f}")
